75e04e07c45a9cd2da51d39cb9ecc64a2d765d11
MK Hejazy <ultimate.zero1994@gmail.com>
2017-05-06 13:51:27 +0200

    fix test

--- src/test/java/test/ServerTest.java
+++ src/test/java/test/ServerTest.java
@@ -26,7 +26,9 @@ public class ServerTest {
 	@AfterClass
 	public static void tearDown() {
 		System.out.println("Tearing down...");
-		server.stop();
+//		server.stop();
+		System.exit(0);
+		System.out.println("Done");
 	}
 
 	@Test
