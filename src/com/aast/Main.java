1fcae0cf89d35881b31b90d641bd2176b120f42b
MK Hejazy <ultimate.zero1994@gmail.com>
2017-05-06 05:05:06 +0200

    test4

--- src/com/aast/Main.java
+++ src/com/aast/Main.java
@@ -1,3 +1,5 @@
+package com.aast;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/com/aast/Server.java
@@ -0,0 +1,20 @@
+package com.aast;
+
+/**
+ * Created by UltimateZero on 5/6/2017.
+ */
+public class Server {
+	private int id;
+
+	public Server(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
